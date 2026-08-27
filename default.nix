b8aacd37d05486e9aa3239245494edbb984ba7a4
Skye J <skye@skyenet.online>
2026-08-27 13:43:16 -0400

    feat: various improvements

--- default.nix
+++ default.nix
@@ -1,44 +1,81 @@
 {
-  local_domain ? "local",
+  local_domain ? "local", # Kept as "local" to avoid leaking tailnet
+  pkgs ? <nixpkgs>,
 }:
-{
-  asticassia = {
-    mainUser = "skye";
-    fqdn = [
-      "asticassia.${local_domain}"
-      "skyenet.online"
-      "skyejonke.com"
-    ];
-    system = "x86_64-linux";
-    tags = [
-      "server"
-      "low power"
-      "public"
-    ];
+let
+  inherit (pkgs) lib;
+  inherit (builtins) elem;
+  inherit (lib) filterAttrs mapAttrsToList lists;
+
+  # Checks if the given subset is a subset of list
+  subset =
+    subset: list:
+    (lists.all (x: x) # Check that all values of the list are true
+      # Map each value in subset to the value true if its in the list or false if it isn't
+      (map (v: (elem v list)) subset)
+    );
+  mkHost =
+    {
+      name,
+      system,
+      hardware ? null,
+      extraFQDNs ? [ ],
+      tags ? [ ],
+      primaryUser ? "skye",
+    }:
+    {
+      inherit
+        name
+        system
+        primaryUser
+        hardware
+        ;
+      # Add system as a tag
+      tags = [ system ] ++ tags;
+      fqdn = [ "${name}.${local_domain}" ] ++ extraFQDNs;
+    };
+in
+rec {
+  hosts = {
+    asticassia = mkHost {
+      primaryUser = "skye";
+      extraFQDNs = [
+        "skyenet.online"
+        "skyejonke.com"
+      ];
+      system = "x86_64-linux";
+      tags = [
+        "server"
+        "low power"
+        "public"
+      ];
+    };
+    honnoji = mkHost {
+      primaryUser = "skye";
+      system = "x86_64-linux";
+      tags = [
+        "server"
+        "monitoring"
+        "builder"
+      ];
+    };
+    lydian = mkHost {
+      primaryUser = "skye";
+      system = "aarch64-darwin";
+      tags = [
+        "desktop"
+      ];
+    };
+    yubiwa = mkHost {
+      primaryUser = "ii69854";
+      system = "aarch64-darwin";
+      tags = [
+        "desktop"
+        "work"
+      ];
+    };
   };
-  honnoji = {
-    mainUser = "skye";
-    system = "x86_64-linux";
-    fqdn = [ "honnoji.${local_domain}" ];
-    tags = [
-      "server"
-    ];
-  };
-  lydian = {
-    mainUser = "skye";
-    system = "aarch64-darwin";
-    fqdn = [ "lydian.${local_domain}" ];
-    tags = [
-      "desktop"
-    ];
-  };
-  yubiwa = {
-    mainUser = "ii69854";
-    system = "aarch64-darwin";
-    fqdn = [ "yubiwa.${local_domain}" ];
-    tags = [
-      "desktop"
-      "work"
-    ];
-  };
+  withTags = tags: (filterAttrs (_: host: subset tags host.tags) hosts);
+  hostHasTag = host: tag: (elem tag hosts.${host}.tags);
+  hostHasTags = host: tag: (subset hosts.${host}.tags tag);
 }
